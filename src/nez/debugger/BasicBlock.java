09f6a0826abd2a9d3be47e9ae2399e459f7145a7
shun-honda <shun.honda.suffix@gmail.com>
2015-07-03 23:42:14 +0900

    added debug vm BasicBlock class

---
+++ src/nez/debugger/BasicBlock.java
@@ -0,0 +1,63 @@
+package nez.debugger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BasicBlock {
+	int codePoint;
+	List<Instruction> insts;
+	List<BasicBlock> preds;
+	List<BasicBlock> succs;
+	public BasicBlock() {
+		this.insts = new ArrayList<Instruction>();
+	}
+	
+	public Instruction get(int index) {
+		return this.insts.get(index);
+	}
+	
+	public BasicBlock append(Instruction inst) {
+		this.insts.add(inst);
+		return this;
+	}
+	
+	public BasicBlock add(int index, Instruction inst) {
+		this.insts.add(index, inst);
+		return this;
+	}
+	
+	public Instruction remove(int index) {
+		return this.insts.remove(index);
+	}
+	
+	public int size() {
+		return this.insts.size();
+	}
+	
+	public int indexOf(Instruction inst) {
+		return this.insts.indexOf(inst);
+	}
+	
+	public void stringfy(StringBuilder sb) {
+		for(int i = 0; i < this.size(); i++) {
+			this.get(i).stringfy(sb);
+			sb.append("\n");
+		}
+	}
+	
+	public List<BasicBlock> getPredecessors() {
+		return this.preds;
+	}
+	
+	public List<BasicBlock> getSuccessors() {
+		return this.succs;
+	}
+	
+	public BasicBlock getSingleSuccessor() {
+		return this.succs.get(0);
+	}
+	
+	public BasicBlock getFailSuccessor() {
+		return this.succs.get(1);
+	}
+}
