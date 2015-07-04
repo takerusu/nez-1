5f9e1b49ebc62b2ba65e7128a69dd91a859baed2
shun-honda <shun.honda.suffix@gmail.com>
2015-07-04 16:30:15 +0900

    updated debug vm Context

--- src/nez/debugger/Context.java
+++ src/nez/debugger/Context.java
@@ -3,6 +3,52 @@ package nez.debugger;
 import nez.ast.Source;
 
 public abstract class Context implements Source {
+	long pos;
+	long longest_pos;
+	boolean result;
+	StackEntry[] stack = null;
+	int StackTop;
+	private static int StackSize = 128;
+
+	public final void initContext() {
+		this.result = true;
+		this.stack = new StackEntry[StackSize];
+		this.stack[0].jump = new Iexit(null);
+		this.StackTop = 0;
+	}
+
+	public final long getPosition() {
+		return this.pos;
+	}
+
+	final void setPosition(long pos) {
+		this.pos = pos;
+	}
+
+	public boolean hasUnconsumed() {
+		return this.pos != length();
+	}
+
+	public final boolean consume(int length) {
+		this.pos += length;
+		return true;
+	}
+
+	public final void rollback(long pos) {
+		if (this.longest_pos < this.pos) {
+			this.longest_pos = this.pos;
+		}
+		this.pos = pos;
+	}
+
+	public final String getSyntaxErrorMessage() {
+		return this.formatPositionLine("error", this.longest_pos, "syntax error");
+	}
+
+	public final String getUnconsumedMessage() {
+		return this.formatPositionLine("unconsumed", this.pos, "");
+	}
+
 	public final Instruction opIexit(Iexit inst) {
 		return null;
 	}
@@ -54,4 +100,9 @@ public abstract class Context implements Source {
 	public final Instruction opIany(Iany inst) {
 		return null;
 	}
+}
+
+class StackEntry {
+	Instruction jump;
+	long pos;
 }
